54a05c0288466751b8d5feb5e504662ba5c2a315
唐瑜 <tangyu@uccc.cc>
2016-11-09 20:22:24 +0800

    完成THYMELEAF模版引擎整合，完成devtools添加

--- boot-study/src/main/java/com/tangyu/web/HomeController.java
+++ boot-study/src/main/java/com/tangyu/web/HomeController.java
@@ -1,20 +1,22 @@
 package com.tangyu.web;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	@ResponseBody
-	public String test() {
+	public String test(Model model,HttpServletRequest request) {
 		logger.info("get request!");
-		return "您好！欢迎来到唐瑜的网页，功能持续集成中，敬请期待！";
+		model.addAttribute("ip", request.getRemoteAddr());
+		return "index";
 	}
 }
