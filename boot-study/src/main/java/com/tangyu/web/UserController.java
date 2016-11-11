408ae913364ec0e31feca71d35e3fa59abd7853b
唐瑜 <tangyu@uccc.cc>
2016-11-11 14:04:54 +0800

    添加文件

--- boot-study/src/main/java/com/tangyu/web/UserController.java
+++ boot-study/src/main/java/com/tangyu/web/UserController.java
@@ -2,9 +2,12 @@ package com.tangyu.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.tangyu.model.User;
 import com.tangyu.service.UserService;
 
 @Controller
@@ -15,10 +18,23 @@ public class UserController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String index() {
-		return "";
+		return "/user/index";
 	}
-	@RequestMapping(value="new",method = RequestMethod.GET)
+
+	@RequestMapping(value = "new", method = RequestMethod.GET)
 	public String newPage() {
 		return "/user/new";
+	}
+
+	@RequestMapping(value = "new", method = RequestMethod.POST)
+	public String saveNew() {
+		return "redirect:/users";
+	}
+
+	@RequestMapping(value = "/{userId}", method = RequestMethod.POST)
+	public String show(@PathVariable Long userId, Model model) {
+		User user = userService.queryById(userId);
+		model.addAttribute("user", user);
+		return "/user/show";
 	}
 }
