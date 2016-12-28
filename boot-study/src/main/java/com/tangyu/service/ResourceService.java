efeaf5cc488a2d7b7b12f24dde98e41b01120f80
唐瑜 <tangyu@uccc.cc>
2016-12-28 16:43:21 +0800

    删除多余文件

--- boot-study/src/main/java/com/tangyu/service/ResourceService.java
+++
@@ -1,5 +0,0 @@
-package com.tangyu.service;
-
-public interface ResourceService {
-
-}
--- boot-study/src/main/resources/static/assets/ty/js/index.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-	initCode(window.location.protocol + '//' + window.location.host ,'token','img');
-	//getCode();
-});
-function refreashCode1() {
-	refreashCode();
-}
